db623a668af6ef18a757a72d6f84565f1da04441
Sarang Sohail <sarangsohail@gmail.com>
2021-03-17 12:03:43 +0000

    Update securitygroup.tf

--- IaC/securitygroup.tf
+++ IaC/securitygroup.tf
@@ -51,3 +51,21 @@ resource "aws_security_group" "elb-securitygroup" {
     Name = "elb"
   }
 }
+
+resource "aws_security_group" "bastion-SG" {
+  name = "bastion-SG"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+  }
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+  }
+  }
+
