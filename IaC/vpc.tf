15485b23763125ec961e875f8b6a8030f5f3fd48
sarangsohail <sarangsohail@gmail.com>
2021-03-17 08:05:34 +0000

    modified private subnet's gateway route

--- IaC/vpc.tf
+++ IaC/vpc.tf
@@ -57,6 +57,7 @@ resource "aws_nat_gateway" "gw" {
 resource "aws_eip" "NAT-EIP" {
   vpc      = true
 }
+
 # route table public
 resource "aws_route_table" "main-public" {
   vpc_id = aws_vpc.main.id
@@ -75,7 +76,7 @@ resource "aws_route_table" "custom-private" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main-gw.id
+    gateway_id = aws_nat_gateway.gw.id
   }
 
   tags = {
